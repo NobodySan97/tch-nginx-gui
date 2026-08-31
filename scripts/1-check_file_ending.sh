d1ff738b57ecfcfd792c8b4e449724023e257e55
NobodySan97 <leo.sanacore@gmail.com>
2026-08-31 14:24:13 +0200

    fix(scripts): modernize shell arithmetic syntax to POSIX \ in 1-check_file_ending.sh

--- scripts/1-check_file_ending.sh
+++ scripts/1-check_file_ending.sh
@@ -8,7 +8,7 @@ check_file_ending() {
 			check_file_ending $file
 		else
 			if [ -f $file ]; then
-				nfile=$[$nfile +1]
+				nfile=$((nfile + 1))
 				echo -ne 'File scanned: '$nfile'\r'
 				if [ $( dos2unix -ic $file ) ]; then
 					dos2unix $file
